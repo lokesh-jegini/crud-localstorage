bbc9469f6d159744722e8fda830eec2d3d5f03ee
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-05-07 23:50:30 +0530

    ip extract  js what is the use of name attribute

--- crud.js
+++ crud.js
@@ -1,4 +1,21 @@
 //onsubmit function
 function datasubmit() {
-    alert("click function work")
+    debugger
+    var data = retrivedata() //function call did not consist dots
+    // console.log(data)
+    var readData=storeDataFromLocalStorage(data)
+}
+//retrive data from the form input (in react this is belong to the onchage (setstate method) name=value,in react we didnot use docuemnt.get method we use only state concept)
+//what is the use of name attribue in js (in react js we use that for hold user input values)
+function retrivedata() {
+    var name1 = document.getElementById('name').value
+    var job1 = document.getElementById('job').value
+    var exp1 = document.getElementById('exp').value
+    var arr = [name1, job1, exp1]
+    return(arr)
+}
+
+//store data to localstorage
+function storeDataFromLocalStorage() {
+
 }
